6b2aa3b7ada8792a2992da80987ee5714abdbbf5
millard2santos <millard2santos@gmail.com>
2023-03-28 19:50:29 +0200

    doing main

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -2,6 +2,14 @@ import React from 'react'
 import './Main.css'
 export const Main = () => {
   return (
-    <div className='main'>Main</div>
+    <div className='main'>
+        <div className='sideMenu'>
+
+        </div>
+        <div className='taskMenu'>
+            <h2>All Tasks</h2>
+            <input type="text" id='task' name='task' placeholder="Add a new task inside 'All' category!" />
+        </div>
+    </div>
   )
 }
